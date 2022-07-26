8106b87d96132076d83b4e36d7a1541911cbecbf
alexandre-notor <amanichambu48@gmail.com>
2022-07-26 10:20:47 +0200

    ajout: configuration de firebase

---
+++ src/component/Firebase/firebase.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import React, { Component } from 'react';
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const config = {
+    apiKey: "AIzaSyAT9rup07gfq3_wFVwQrIS-eRPzIIaLjB4",
+    authDomain: "marvel-quiz-b4c1c.firebaseapp.com",
+    projectId: "marvel-quiz-b4c1c",
+    storageBucket: "marvel-quiz-b4c1c.appspot.com",
+    messagingSenderId: "940096428643",
+    appId: "1:940096428643:web:5d21d40dd7f932bf7fb656"
+};
+
+class Firebase extends Component {
+
+    constructor() {
+        super();
+        // Initialisation de Firebase dans notre application
+        this.app = initializeApp(config);
+        // Implementation de l'API d'authentification
+        this.auth = getAuth();
+        // 
+        this.user = null;
+    }
+
+    // Defifinition de differente fonctions d'authentification
+
+    // l'inscription
+    signupUser(email, password) {
+        createUserWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+                this.user = userCredential.user;
+            })
+            .catch((error) => {
+
+            })
+    }
+
+    // la connexion
+    loginUser(email, password) {
+        signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                this.user = userCredential.user;
+            })
+            .catch((error) => {
+                
+            })
+    }
+
+    // la deconnexion
+    logoutUser() {
+        this.auth.signOut();
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App';
+import Firebase, { FirebaseContext } from './component/Firebase';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
